57a08b758deb4bedf70ead0d0a34fd04668321b7
Matthew Peterson <peterson908@student.pcsstn.net>
2021-02-23 15:30:28 +0000

    Developed version 1 using recursion

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
-int main() {
-  int num = 0;
-  int sum = 0;
-  int t = 0;
-  printf("Enter a number: \n");
-  scanf("%d", &num);
-  while(num > 0) {
-    t = num % 10; //t = remainder after dividing by 10
-    sum = sum + t; //sum (0) plus remainder after dividing by 10
-    num = num/10; //divides input by 10
-  }
-printf("Sum: %d\n", sum);
-return 0;
-}
+int sum(int num);
 
+int main() {
+  int number;
+  int result;
+  printf("Enter a number: \n");
+  scanf("%d", &number); // Number = input
+  result = sum(number); // Result = sum() output
+  printf("Sum = %d", result); //Print result from sum() function
+  return 0;
+}
+int sum(int num) {
+  if(num != 0) {
+    return num + sum(num-1);
+  } else {
+    return num; }
+}
